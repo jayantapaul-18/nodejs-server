d70402784fe4cf3998512f6e53b7ce36996534e6
Jayanta Paul <jayantapaul-18@users.noreply.github.com>
2020-03-08 05:02:12 -0700

    added logger

--- server.js
+++ server.js
@@ -1,14 +1,47 @@
 const express = require('express');
-const server = express();
+const app = express();
 const os = require('os');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const helmet = require('helmet')
 const api = require('./api');
-//server.use(api,api);
-server.use('*',function(request,response,next){
-    response.header('supper-server','supper-server');
-    response.setHeader('supper-X','supper-X');
+//app.use(api,api);
+const SERVER_PORT = 3005;
+const winston = require('winston');
+const logger = winston.createLogger({
+    level: 'info',
+    format:winston.format.combine(
+           winston.format.timestamp({
+           format: 'YYYY-MM-DD HH:mm:ss'
+        }),
+        winston.format.errors({ stack: true }),
+        winston.format.splat(),
+        winston.format.json()
+      ),
+    defaultMeta: { service: 'Node' },
+    transports: [
+      new winston.transports.File({ filename: 'error.log', level: 'error' }),
+      new winston.transports.File({ filename: 'combined.log' })
+    ]
+  });
+  if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+      format: winston.format.simple()
+    }));
+  }
+
+app.use(helmet())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use('*',(req,res,next)=>{
+    res.header('supper-server','supper-server');
+    res.setHeader('supper-X','supper-X');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next()
 })
-server.get('/app/healthcheck',function(request,response){
+
+app.all('/app/healthcheck',(req,res)=>{
     var health = {
         "status":"up",
         "uptime":process.uptime(),
@@ -17,10 +50,10 @@ server.get('/app/healthcheck',function(request,response){
         "freemem":os.freemem(),
         "networkInterfaces":os.networkInterfaces()
     }
-    response.send(health);
+    res.send(health);
 })
 
-server.all('/app/name',function(request,response){
+app.all('/app/name',(req,res)=>{
     var hostName =os.hostname();
     const Body = {
         "server": process.versions.node,
@@ -28,9 +61,16 @@ server.all('/app/name',function(request,response){
         "hostName":hostName,
         "osType":os.type()
     }
-    response.send(Body);
+    res.send(Body);
 })
 
-server.listen(process.env.PORT || 3005,process.env.IP ||  "0.0.0.0" ,process.env.address || 'localhost', function(){
-    console.log('Server Started on http://localhost:3005');
+app.listen(process.env.SERVER_PORT || SERVER_PORT,process.env.IP ||  "0.0.0.0" ,process.env.address || 'localhost', function(){
+    console.log('Server Started on http://localhost:'+SERVER_PORT+'/app/healthcheck');
+    console.log('Server Started on http://localhost:'+SERVER_PORT+'/app/name');
+    logger.log({
+        level: 'info',
+        message: 'Server Started on http://localhost:'+SERVER_PORT,
+        additional: 'properties',
+        are: 'passed along'
+      });
 })
